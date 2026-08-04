b8146057c0f258a7e4891ed51cfa291616754577
alex.zhilinsky2001@gmail.com <alex.zhilinsky2001@gmail.com>
2026-08-04 10:07:19 +0200

    FileHash index added

--- TextAnalyzer.Infrastructure/Migrations/20260804080651_AddFileHashIndex.Designer.cs
+++ TextAnalyzer.Infrastructure/Migrations/20260804080651_AddFileHashIndex.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TextAnalyzer.Infrastructure.Data;
@@ -11,9 +12,11 @@ using TextAnalyzer.Infrastructure.Data;
 namespace TextAnalyzer.Infrastructure.Migrations
 {
     [DbContext(typeof(TextAnalyzerDbContext))]
-    partial class TextAnalyzerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260804080651_AddFileHashIndex")]
+    partial class AddFileHashIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
